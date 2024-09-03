269bbf087fcc9b04029403decb9be0682a01b98e
Emma Ahmad <emmaahmadsir@gmail.com>
2024-09-04 00:16:30 +0800

    fix migration

--- db/migration/20240816012255_initial.sql
+++ db/migration/20240816012255_initial.sql
@@ -40,7 +40,7 @@ CREATE TABLE "summary" (
   "doc_id" bigserial NOT NULL,
   "param1" bool NOT NULL DEFAULT false,
   "param2" bool NOT NULL DEFAULT false,
-  "summary" BYTEA NOT NULL DEFAULT "no summary"
+  "summary" BYTEA
 );
 CREATE TABLE"sessions"("id"uuid PRIMARY KEY,"username"varchar NOT NULL,"refresh_token"varchar NOT NULL,"user_agent"varchar NOT NULL,"client_ip"varchar NOT NULL,"is_blocked"boolean NOT NULL DEFAULT false,"expires_at"timestamptz NOT NULL,"created_at"timestamptz NOT NULL DEFAULT (now()) );
 
