7000910cc5d491a0c9e82ac1bed521836dbef559
Emma Ahmad <emmaahmadsir@gmail.com>
2024-09-29 18:10:35 +0800

    wip

--- gopsql.sh
+++ gopsql.sh
@@ -1,1 +1,3 @@
-psql postgresql://emma:happy@localhost:5434/summafy?sslmode=disable
+psql postgresql://emma:happybirthday@my-rds.cnieu4oowqof.us-east-1.rds.amazonaws.com:5432/summafy?sslmode=allow
+
+ssh -N -L 5436:summafy.my-rds.cnieu4oowqof.us-east-1.rds.amazonaws.com:5432 -p 22 -i mykey.pem ec2-user@107.23.208.53
